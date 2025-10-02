3474714a9615b05f8fb8cb06731ca5c9edf4a14b
Bouchiba43 <bouchibaahmed43@gmail.com>
2025-10-02 20:52:14 +0100

    defined p2p intefaces

---
+++ p2p/transport.go
@@ -0,0 +1,13 @@
+package p2p
+
+
+
+//Peer is an interface that reprensents the remote node 
+type Peer interface {
+}
+
+
+//Transport is anything that handles communication 
+//between the nodes in the network .This can be of the form (TCP, UDP, websockets,...)
+type Transport interface {
+}
